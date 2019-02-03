da2adb9da0462db85c542d0ad60070157862e0f1
Cecilia Wong <ceciliakhwong@gmail.com>
2019-02-03 18:45:57 -0500

    Adds User model queries

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const users = require('./users')
 
 app.use(bodyParser.json())
 app.use(
@@ -14,6 +15,8 @@ app.get('/', (request, response) => {
   response.json({ info: 'Welcome to The Store!'})
 })
 
+app.get('/users', users.getUsers)
+app.get('/users/:id', users.getUserById)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
