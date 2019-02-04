0207307e634e929e6ea0df2110e40b53c05df3c5
Cecilia Wong <ceciliakhwong@gmail.com>
2019-02-03 22:51:50 -0500

    Adds Order Items model queries

--- app/index.js
+++ app/index.js
@@ -8,6 +8,7 @@ const items = require('./items')
 const paymentMethods = require('./payment-methods')
 const categories = require('./categories')
 const orders = require('./orders')
+const orderItems = require('./order-items')
 
 app.use(bodyParser.json())
 app.use(
@@ -16,30 +17,44 @@ app.use(
   })
 )
 
+// General 
 app.get('/', (request, response) => {
   response.json({ info: 'Welcome to The Store!'})
 })
 
+// Users
 app.get('/users', users.getUsers)
 app.get('/users/:id', users.getUserById)
 
+// Items
 app.get('/items', items.getItems)
 app.get('/items/:id', items.getItemById)
 
+// Payment Methods
 app.get('/paymentmethods', paymentMethods.getPaymentMethods)
 app.get('/paymentmethods/:id', paymentMethods.getPaymentMethodById)
 
+// Categories
 app.get('/categories', categories.getCategories)
 app.get('/categories/:id', categories.getCategoryById)
 
+// Orders
 app.get('/orders', orders.getOrders)
 app.get('/orders/:id', orders.getOrderById)
 app.post('/orders', orders.createOrder)
 app.put('/orders/:orderId', orders.updateOrder)
 
+// User Orders
 app.get('/users/:userId/orders', orders.getOrdersByUserId)
 app.post('/users/:userId/orders', orders.createUserOrder)
 app.put('/users/:userId/orders/:orderId', orders.updateUserOrder)
+
+// Order Items
+app.get('/orderitems', orderItems.getOrderItems)
+app.get('/orders/:orderId/items', orderItems.getOrderItemsByOrderId)
+app.post('/orders/:orderId/items', orderItems.createOrderItem)
+app.put('/orders/:orderId/items/:orderItemId', orderItems.updateOrderItemQuantity)
+app.delete('/orders/:orderId/items/:orderItemId', orderItems.deleteOrderItem)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
